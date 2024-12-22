d46510e3958e348a4155896ebff2f05ab57cb483
ryanpsouzaa <ryanpsouzagit@gmail.com>
2024-12-22 14:49:45 -0300

    sons adicionados a cada tecla

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-alert("Olá Mundo");
-document.querySelector('.tecla_pom');
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-};
+function tocaSom(idSomElemento){
+    document.querySelector(idSomElemento).play();
+}
 
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
+//criando a lista com todas as teclas -> 9 teclas
+const listaTeclas = document.querySelectorAll('.tecla');
+
+
+for(let contador = 0; contador < listaTeclas.length; contador++) {
+
+    const tecla = listaTeclas[contador];
+    const som = tecla.classList[1];
+
+    //template string
+    const id = `#som_${som}`;
+
+    tecla.onclick = function(){
+        tocaSom(id);
+    }
+}
